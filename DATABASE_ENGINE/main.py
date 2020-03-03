7cfd84c0b9be8aebe474207670c503c67ffa19ca
Sahil Ashar <sahilashar16@gmail.com>
2020-03-03 15:46:11 -0600

    very skeleton framework for dynamic urls

--- DATABASE_ENGINE/main.py
+++ DATABASE_ENGINE/main.py
@@ -16,6 +16,64 @@ def root():
 
     return render_template('index.html', times=dummy_times)
 
+@app.route('/about')
+def about():
+    # For the sake of example, use static information to inflate the template.
+    # This will be replaced with real information in later steps.
+    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
+                   datetime.datetime(2018, 1, 2, 10, 30, 0),
+                   datetime.datetime(2018, 1, 3, 11, 0, 0),
+                   ]
+
+    return render_template('index.html', times=dummy_times)
+
+
+@app.route('/years/')
+@app.route('/years/<year>/')
+def year_root(year=None):
+    # For the sake of example, use static information to inflate the template.
+    # This will be replaced with real information in later steps.
+    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
+                   datetime.datetime(2018, 1, 2, 10, 30, 0),
+                   datetime.datetime(2018, 1, 3, 11, 0, 0),
+                   ]
+
+    return render_template('index.html', times=dummy_times)
+
+@app.route('/awards/')
+def award_root():
+    # For the sake of example, use static information to inflate the template.
+    # This will be replaced with real information in later steps.
+    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
+                   datetime.datetime(2018, 1, 2, 10, 30, 0),
+                   datetime.datetime(2018, 1, 3, 11, 0, 0),
+                   ]
+
+    return render_template('index.html', times=dummy_times)
+
+
+@app.route('/people/')
+def people_root():
+    # For the sake of example, use static information to inflate the template.
+    # This will be replaced with real information in later steps.
+    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
+                   datetime.datetime(2018, 1, 2, 10, 30, 0),
+                   datetime.datetime(2018, 1, 3, 11, 0, 0),
+                   ]
+
+    return render_template('index.html', times=dummy_times)
+
+@app.route('/movies/')
+def movies_root():
+    # For the sake of example, use static information to inflate the template.
+    # This will be replaced with real information in later steps.
+    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
+                   datetime.datetime(2018, 1, 2, 10, 30, 0),
+                   datetime.datetime(2018, 1, 3, 11, 0, 0),
+                   ]
+
+    return render_template('index.html', times=dummy_times)
+
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
