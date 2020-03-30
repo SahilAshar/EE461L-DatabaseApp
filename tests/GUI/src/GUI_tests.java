5d680a91b63b1dbfeddebf321aa1d734d991f8d0
Noah <noahlisk@utexas.edu>
2020-03-30 13:44:53 -0500

    Selenium tests

--- tests/GUI/src/GUI_tests.java
+++ tests/GUI/src/GUI_tests.java
@@ -212,4 +212,56 @@ public class GUI_tests {
         TimeUnit.SECONDS.sleep(2);
         wd.quit(); // close the browser window
     }
+
+     @Test
+    public void people_years() throws InterruptedException {
+        System.setProperty("webdriver.gecko.driver", "tests/GUI/geckodriver.exe");
+        WebDriver wd = new FirefoxDriver(); // launch the browser
+        wd.get("https://databaseengine.appspot.com/");
+        WebElement we = wd.findElement(By.xpath("//*[contains(text(),'Splash')]"));
+        we.click(); //click the button
+        TimeUnit.SECONDS.sleep(2);
+        we = wd.findElement(By.xpath("//*[contains(text(),'Awards')]"));
+        we.click(); //click the button
+        TimeUnit.SECONDS.sleep(2);
+        we = wd.findElement(By.xpath("//*[contains(text(),'Directing')]"));
+        we.click(); //click the button
+        TimeUnit.SECONDS.sleep(2);
+        we = wd.findElement(By.xpath("//*[contains(text(),'Years')]"));
+        we.click(); //click the button
+        TimeUnit.SECONDS.sleep(2);
+        we = wd.findElement(By.xpath("//*[contains(text(),'1997')]"));
+        we.click(); //click the button
+        TimeUnit.SECONDS.sleep(2);
+        we = wd.findElement(By.xpath("//*[contains(text(),'About')]"));
+        we.click(); //click the button
+        TimeUnit.SECONDS.sleep(2);
+        wd.quit(); // close the browser window
+    }
+
+     @Test
+    public void years_people() throws InterruptedException {
+        System.setProperty("webdriver.gecko.driver", "tests/GUI/geckodriver.exe");
+        WebDriver wd = new FirefoxDriver(); // launch the browser
+        wd.get("https://databaseengine.appspot.com/");
+        WebElement we = wd.findElement(By.xpath("//*[contains(text(),'Splash')]"));
+        we.click(); //click the button
+        TimeUnit.SECONDS.sleep(2);
+        we = wd.findElement(By.xpath("//*[contains(text(),'Awards')]"));
+        we.click(); //click the button
+        TimeUnit.SECONDS.sleep(2);
+        we = wd.findElement(By.xpath("//*[contains(text(),'Directing')]"));
+        we.click(); //click the button
+        TimeUnit.SECONDS.sleep(2);
+        we = wd.findElement(By.xpath("//*[contains(text(),'Years')]"));
+        we.click(); //click the button
+        TimeUnit.SECONDS.sleep(2);
+        we = wd.findElement(By.xpath("//*[contains(text(),'1997')]"));
+        we.click(); //click the button
+        TimeUnit.SECONDS.sleep(2);
+        we = wd.findElement(By.xpath("//*[contains(text(),'About')]"));
+        we.click(); //click the button
+        TimeUnit.SECONDS.sleep(2);
+        wd.quit(); // close the browser window
+    }
 }
